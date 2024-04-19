9fb0cd9672450bfdafdd5b43bcf496ce6876344f
Soham Kolhatkar <sohamk130405@gmail.com>
2024-04-19 09:56:55 +0530

    Added address in database field of users

--- server/models/user.js
+++ server/models/user.js
@@ -37,6 +37,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: "false",
   },
+  coinAddress: {
+    type: String,
+    default: "",
+  },
 });
 
 const User = mongoose.model("User", userSchema);
